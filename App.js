07f2cd5a20b5ae4ef89fd109ac7f05af717305e2
nehajetto <nehajetto@gmail.com>
2022-06-28 10:38:01 +0530

    2 pages

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import {View,Text} from 'react-native';
+import React from 'react';
+import Page2 from './src/containers/page2';
+import Page3 from './src/containers/page3';
+
+const App=()=>
+{
+	
+	return(
+	
+		<Page3/>
+	  )
+}
+
+export default App;
---
+++ src/containers/page3/index.js
@@ -0,0 +1,60 @@
+import {View,Text,TouchableOpacity,StyleSheet,Dimensions} from 'react-native';
+import React from 'react';
+
+const{width,height}=Dimensions.get('window');
+
+const Page3=()=>
+{
+  return(
+      <View style={styles.container}>
+
+
+          <TouchableOpacity style={styles.bluebn}>
+            <Text style ={styles.mainTxt}>
+                DOCTORS
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.bluebn}>
+            <Text style ={styles.mainTxt}>
+                LAB FACILITY
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.bluebn}>
+            <Text style ={styles.mainTxt}>
+                PALLIATIVE CARE
+            </Text>
+          </TouchableOpacity>
+ 
+          
+      
+
+      </View>
+  )
+
+}
+const styles=StyleSheet.create({
+    container:{
+         padding:40
+             
+    },
+    bluebn:{
+        padding:25,
+        shadowColor: 'black',
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 10,
+        elevation: 3,
+        backgroundColor: 'white',
+        borderRadius:20,
+        marginTop:50
+
+    },
+    mainTxt:{
+        textAlign:'center'
+    }
+
+})
+export default Page3;
